5a0b51c4bf32de17b0c629a657329a8fe7e4971f
wagnerferreiraf <professorwagnerferreira@gmail.com>
2025-11-14 11:45:10 -0400

    Aula do dia 14/11/2025

--- src/app/msgs/page.js
+++ src/app/msgs/page.js
@@ -2,6 +2,10 @@ export default function Msgs() {
     return (
         <div>
             <h1>Mensagens</h1>
+
+            {/* exiba todas as mensagens da rota abaixo em uma tabela
+            https://api.front.dev.vilhena.ifro.edu.br/msgs
+            */}
         </div>
     )
 }
